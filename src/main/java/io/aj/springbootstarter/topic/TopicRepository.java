fd421b319d0e3e88af132d4bcfbb418bb91b0ef2
akshat12345 <akshatjindal1993@gmail.com>
2017-03-05 23:31:09 +0530

    Course Api Using Spring Boot

---
+++ src/main/java/io/aj/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,10 @@
+package io.aj.springbootstarter.topic;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String> {
+
+	
+}
